4e771c19d3d85cc79688396e829b6fd72c362fc9
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-14 13:12:23 +0200

    Export some utils

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ export * from './deterministic-wallet';
 export * from './implementations';
 export * from './dpaths';
 export * from './types';
+export {
+  getFullPath,
+  getPathPrefix,
+  toChecksumAddress,
+  stripHexPrefix,
+  addHexPrefix
+} from './utils';
