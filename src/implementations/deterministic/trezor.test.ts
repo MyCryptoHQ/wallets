c2776ed75013a066e1f04ec4253effa5b14c68c3
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-22 12:01:40 +0200

    Add test for getAddressesWithMultipleDPaths

--- src/implementations/deterministic/trezor.test.ts
+++ src/implementations/deterministic/trezor.test.ts
@@ -221,6 +221,70 @@ describe('TrezorWallet', () => {
     });
   });
 
+  describe('getAddressesWithMultipleDPaths', () => {
+    it('fetches multiple addresses for multiple derivation paths', async () => {
+      const wallet = new TrezorWallet(manifest);
+
+      await expect(
+        wallet.getAddressesWithMultipleDPaths([
+          { path: DEFAULT_ETH, limit: 5 },
+          { path: LEDGER_LIVE_ETH, limit: 5 }
+        ])
+      ).resolves.toStrictEqual([
+        expect.objectContaining({
+          address: '0xc6D5a3c98EC9073B54FA0969957Bd582e8D874bf',
+          dPath: "m/44'/60'/0'/0/0",
+          index: 0
+        }),
+        expect.objectContaining({
+          address: '0x59A897A2dbd55D20bCC9B52d5eaA14E2859Dc467',
+          dPath: "m/44'/60'/0'/0/1",
+          index: 1
+        }),
+        expect.objectContaining({
+          address: '0x7D5e716Bbc8771af9c5ec3b0555B48a4a84d4ba7',
+          dPath: "m/44'/60'/0'/0/2",
+          index: 2
+        }),
+        expect.objectContaining({
+          address: '0x8137eC5954A8ed45A90F3bd58f717228b5670858',
+          dPath: "m/44'/60'/0'/0/3",
+          index: 3
+        }),
+        expect.objectContaining({
+          address: '0xc0C386F7f0B02FAC0d63B2de00a01e77992B011B',
+          dPath: "m/44'/60'/0'/0/4",
+          index: 4
+        }),
+        expect.objectContaining({
+          address: '0xc6D5a3c98EC9073B54FA0969957Bd582e8D874bf',
+          dPath: "m/44'/60'/0'/0/0",
+          index: 0
+        }),
+        expect.objectContaining({
+          address: '0x3FE703a2035CB3590C865a09F556eDda02b2Cf12',
+          dPath: "m/44'/60'/1'/0/0",
+          index: 1
+        }),
+        expect.objectContaining({
+          address: '0x2159a414C4Ac080482CE6F942cc5BC59306a1A47',
+          dPath: "m/44'/60'/2'/0/0",
+          index: 2
+        }),
+        expect.objectContaining({
+          address: '0xBdec34481829c2396FA15bAc5227D0B05cF3Dc41',
+          dPath: "m/44'/60'/3'/0/0",
+          index: 3
+        }),
+        expect.objectContaining({
+          address: '0x91e2831A52E4eeE2efa8B05B0bD0C930407495DC',
+          dPath: "m/44'/60'/4'/0/0",
+          index: 4
+        })
+      ]);
+    });
+  });
+
   describe('getExtendedKey', () => {
     it('fetches a public key and chaincode from the device', async () => {
       const wallet = new TrezorWallet(manifest);
