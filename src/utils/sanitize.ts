609d8d25a861827386c2a8b1ebcce5982c79ea00
Frederik Bolding <frederik.bolding@gmail.com>
2021-07-26 11:31:10 +0200

    Simplify sanitize function

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -3,11 +3,6 @@ import { BigNumber } from '@ethersproject/bignumber';
 import type { UnsignedTransaction } from '@ethersproject/transactions';
 
 export const sanitizeTx = (tx: TransactionRequest): UnsignedTransaction => ({
-  chainId: tx.chainId || undefined,
-  data: tx.data || undefined,
-  gasLimit: tx.gasLimit || undefined,
-  gasPrice: tx.gasPrice || undefined,
-  nonce: tx.nonce ? BigNumber.from(tx.nonce).toNumber() : undefined,
-  to: tx.to || undefined,
-  value: tx.value || undefined
+  ...tx,
+  nonce: tx.nonce ? BigNumber.from(tx.nonce).toNumber() : undefined
 });
