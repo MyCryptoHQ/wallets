a0feb1d46eeb0ea16bd39c8a9407879251ea252b
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-29 13:02:56 +0200

    Work on pairing process

--- src/implementations/deterministic/gridplus.ts
+++ src/implementations/deterministic/gridplus.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-restricted-globals */
 import type { TransactionRequest } from '@ethersproject/abstract-provider';
 import type { SignatureLike } from '@ethersproject/bytes';
 import { hexlify } from '@ethersproject/bytes';
 import { toUtf8Bytes } from '@ethersproject/strings';
 import { serialize as serializeTransaction } from '@ethersproject/transactions';
 import crypto from 'crypto';
+// @ts-expect-error No types right now
 import { Client } from 'gridplus-sdk';
 import { promisify } from 'util';
 
@@ -16,17 +18,79 @@ import { HardwareWallet } from './hardware-wallet';
 
 const HARDENED_OFFSET = 0x80000000;
 
+export interface GridPlusConfiguration {
+  // For client
+  name: string;
+
+  // Identifying the device
+  deviceID?: string;
+  password?: string;
+}
+
 // @todo Handle conection & pairing
 // @todo Figure out how to fetch addresses properly
 
+const getPrivateKey = (config: GridPlusConfiguration) => {
+  const buf = Buffer.concat([
+    Buffer.from(config.password!),
+    Buffer.from(config.deviceID!),
+    Buffer.from(config.name)
+  ]);
+  return crypto.createHash('sha256').update(buf).digest();
+};
+
+const waitForPairing = (config: GridPlusConfiguration) => {
+  return new Promise((resolve, reject) => {
+    const baseURL = 'https://wallet.gridplus.io';
+    const url = `${baseURL}?keyring=${config.name}`;
+
+    const popup = window.open(url)!;
+    popup.postMessage('GET_LATTICE_CREDS', baseURL);
+
+    // PostMessage handler
+    function receiveMessage(event: any) {
+      // Ensure origin
+      if (event.origin !== baseURL) {
+        return;
+      }
+      // Parse response data
+      try {
+        const data = JSON.parse(event.data);
+        if (!data.deviceID || !data.password) {
+          return reject(Error('Invalid credentials returned from Lattice.'));
+        }
+        return resolve(data);
+      } catch (err) {
+        return reject(err);
+      }
+    }
+    window.addEventListener('message', receiveMessage, false);
+  });
+};
+
+const ensureConnection = async (client: Client, config: GridPlusConfiguration) => {
+  if (config.deviceID !== undefined && config.password !== undefined) {
+    const connect = promisify(client.connect).bind(client);
+
+    const isPaired: boolean = await connect(config.deviceID);
+    if (isPaired) {
+      return;
+    }
+  }
+
+  // @todo Pairing?
+  return waitForPairing(config);
+};
+
 export class GridPlusWalletInstance implements Wallet {
   constructor(
+    private readonly config: GridPlusConfiguration,
     private readonly client: Client,
     private readonly path: string,
     private address?: TAddress
   ) {}
 
-  private getConvertedPath = () => {
+  private getConvertedPath() {
     const array = this.path.split('/').slice(1);
     return array.map((a) => {
       const isHardened = a.includes("'");
@@ -34,7 +98,7 @@ export class GridPlusWalletInstance implements Wallet {
       const sliced = isHardened ? a.slice(0, a.length - 1) : a;
       return offset + parseInt(sliced);
     });
-  };
+  }
 
   async signTransaction(rawTx: TransactionRequest): Promise<string> {
     const transaction = sanitizeTx(rawTx);
@@ -45,6 +109,8 @@ export class GridPlusWalletInstance implements Wallet {
         WalletsErrorCode.MISSING_ARGUMENTS
       );
     }
+
+    await ensureConnection(this.client, this.config);
 
     const sign = promisify(this.client.sign).bind(this.client);
 
@@ -76,6 +142,9 @@ export class GridPlusWalletInstance implements Wallet {
       payload: msgHex,
       signerPath: this.getConvertedPath()
     };
+
+    await ensureConnection(this.client, this.config);
+
     const sign = promisify(this.client.sign).bind(this.client);
 
     const signed = await sign({
@@ -88,8 +157,9 @@ export class GridPlusWalletInstance implements Wallet {
 
   async getAddress(): Promise<TAddress> {
     if (!this.address) {
-      const addresses = promisify(this.client.addresses).bind(this.client);
-      this.address = (await addresses({
+      await ensureConnection(this.client, this.config);
+      const getAddresses = promisify(this.client.getAddresses).bind(this.client);
+      this.address = (await getAddresses({
         startPath: this.getConvertedPath(),
         n: 1,
         skipCache: true
@@ -104,11 +174,18 @@ export class GridPlusWalletInstance implements Wallet {
 }
 
 export class GridPlusWallet extends HardwareWallet {
-  private client?: Client;
-
-  constructor(config: { name: string; privKey: string }) {
+  constructor(private config: GridPlusConfiguration) {
     super();
-    this.client = new Client({ ...config, crypto });
+  }
+
+  async getClient(): Promise<Client> {
+    const { deviceID, password, ...clientConfig } = this.config;
+    if (deviceID === undefined || password === undefined) {
+      const result: any = await waitForPairing(this.config);
+      this.config = { ...this.config, ...result };
+    }
+    const privKey = getPrivateKey(this.config);
+    return new Client({ ...clientConfig, privKey, crypto });
   }
 
   async getAddress(path: DerivationPath, index: number): Promise<TAddress> {
@@ -116,7 +193,7 @@ export class GridPlusWallet extends HardwareWallet {
     return wallet.getAddress();
   }
 
-  async getExtendedKey(path: string): Promise<{ publicKey: string; chainCode: string }> {
+  async getExtendedKey(_path: string): Promise<{ publicKey: string; chainCode: string }> {
     // @todo
     throw new Error('Method not implemented.');
   }
@@ -127,6 +204,7 @@ export class GridPlusWallet extends HardwareWallet {
   }
 
   async getWallet(path: DerivationPath, index: number, address?: TAddress): Promise<Wallet> {
-    return new GridPlusWalletInstance(this.client, getFullPath(path, index), address);
+    const client = await this.getClient();
+    return new GridPlusWalletInstance(this.config, client, getFullPath(path, index), address);
   }
 }
--- src/implementations/deterministic/index.ts
+++ src/implementations/deterministic/index.ts
@@ -2,3 +2,4 @@ export * from './mnemonic-phrase';
 export * from './hardware-wallet';
 export * from './ledger';
 export * from './trezor';
+export * from './gridplus'
