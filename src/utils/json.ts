d92975d4a07217cfe7354725f41e9d27b35b0466
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-08-04 13:00:52 +0200

    Bump TypeScript version (#35)

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,4 +1,4 @@
-export const safeJSONParse = <T>(str: string): [null, T] | [Error, null] => {
+export const safeJSONParse = <T>(str: string): [null, T] | [unknown, null] => {
   try {
     return [null, JSON.parse(str)];
   } catch (err) {
